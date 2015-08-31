8a7326af482e8669bc1696a053e8cfa273c9ab3e
Matheus Davidson <matheusdavidson@gmail.com>
2015-08-31 18:56:25 -0300

    extent

--- src/axis-x-directive.js
+++ src/axis-x-directive.js
@@ -15,15 +15,15 @@ angular.module('gridshore.c3js.chart')
  *
  * @param {String} axis-position Location of the label. Can have following values:
  *
- *   - Horizontal: inner-right (default), inner-center, inner-left, outer-right, outer-center, outer-left 
+ *   - Horizontal: inner-right (default), inner-center, inner-left, outer-right, outer-center, outer-left
  *   - Vertical: inner-top (default), inner-middle, inner-bottom, outer-top, outer-middle, outer-bottom
- *   
+ *
  *   {@link http://c3js.org/reference.html#data-x| c3js doc}
  * @param {String} axis-label Set the text for the label of the x axis.
  *
  *   {@link http://c3js.org/reference.html#axis-x-label| c3js doc}
  * @param {Number} padding-left Set padding on the left side of the x axis.
- * 
+ *
  *   {@link http://c3js.org/reference.html#axis-x-padding| c3js doc}
  * @param {Number} padding-right Set padding on the right side of the x axis.
  *
@@ -62,42 +62,54 @@ angular.module('gridshore.c3js.chart')
  *   </chart-axis>
  */
 
-function ChartAxisX () {
-    var axisLinker = function (scope, element, attrs, chartCtrl) {
+function ChartAxisX() {
+    var axisLinker = function(scope, element, attrs, chartCtrl) {
         var position = attrs.axisPosition;
         var label = attrs.axisLabel;
 
-        var axis = {"label": {"text": label, "position": position}};
+        var axis = {
+            "label": {
+                "text": label,
+                "position": position
+            }
+        };
 
         var paddingLeft = attrs.paddingLeft;
         var paddingRight = attrs.paddingRight;
         if (paddingLeft || paddingRight) {
             paddingLeft = (paddingLeft) ? paddingLeft : 0;
             paddingRight = (paddingRight) ? paddingRight : 0;
-            axis.padding = {"left": parseInt(paddingLeft), "right": parseInt(paddingRight)};
+            axis.padding = {
+                "left": parseInt(paddingLeft),
+                "right": parseInt(paddingRight)
+            };
         }
-        var height=attrs.axisHeight;
+        var height = attrs.axisHeight;
         if (height) {
             axis.height = parseInt(height);
         }
-        
+
         if (attrs.show === 'false') {
             axis.show = false;
         }
         if (attrs.axisLocaltime === 'true') {
-            axis.localtime=true;
+            axis.localtime = true;
         }
-        var max=attrs.axisMax;
+        var max = attrs.axisMax;
         if (max) {
-            axis.max=max;
+            axis.max = max;
         }
-        var min=attrs.axisMin;
+        var min = attrs.axisMin;
         if (min) {
-            axis.min=min;
+            axis.min = min;
         }
-        var type=attrs.axisType;
+        var type = attrs.axisType;
         if (type) {
-            axis.type=type;   
+            axis.type = type;
+        }
+        var extent = attrs.axisExtent;
+        if (extent) {
+            axis.extent = extent;
         }
         chartCtrl.addAxisProperties('x', axis);
     };
